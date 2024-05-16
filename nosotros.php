ff4632672d3c3174c4cc5a14379526355033ac1c
0101mery0101 <meryrojasquispe41@gmail.com>
2024-05-16 09:02:37 -0500

    nosotros

--- nosotros.php
+++ nosotros.php
@@ -15,49 +15,79 @@
     <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.4.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        .text-justify {
-            text-align: justify;
-        }
 
-        .large-text {
-            font-size: 1.2rem;
-            /* Ajusta este valor según tus necesidades */
-        }
-    </style>
 </head>
 
 <body>
     <?php include ('include/menu.php'); ?>
-    <br><br><br>
-    <div class="container my-5">
-        <div class="row">
-            <div class="col-md-4 d-flex justify-content-center align-items-center">
-                <img src="imagen/imagen.nosotros.jpg"
-                    class="img-fluid" alt="Imagen de Nosotros">
-            </div>
-            <div class="col-md-8">
-                <h2 class="text-center">Sobre Nosotros</h2>
-                <p class="text-justify large-text">
-                En nuestra tienda virtual de zapatillas, nos dedicamos a ofrecerte mucho más que simples zapatos. Somos un destino para aquellos que buscan expresarse a
-                 través de su calzado, para los que desean caminar con confianza y originalidad en cada paso que dan.
 
-                En Zapatillas Únicas, valoramos la individualidad y la diversidad de estilos. Nuestra cuidadosa selección de calzado no solo abarca 
-                las últimas tendencias de moda, sino también diseños únicos y exclusivos que te permitirán destacar entre la multitud.
-                </p>
-                <p class="text-justify large-text">
-                Cada par de zapatillas que ofrecemos cuenta una historia, desde su diseño innovador hasta su fabricación con materiales de la más alta calidad. Nos esforzamos por brindarte una experiencia de compra en línea sin complicaciones, donde puedas explorar, descubrir y encontrar
-                 ese par de zapatillas que no solo completen tu outfit, sino que también reflejen tu personalidad y estilo.
-                </p>
-                <p class="text-justify large-text">
-                Cada par de zapatillas que ofrecemos cuenta una historia, desde su diseño innovador hasta su fabricación con materiales de la más alta calidad. Nos esforzamos por brindarte una experiencia de compra en línea sin complicaciones, donde puedas explorar, descubrir y encontrar
-                 ese par de zapatillas que no solo completen tu outfit, sino que también reflejen tu personalidad y estilo.
-                </p>
+    <div class="main-content">
+
+        <div class="page-content">
+            <div class="container-fluid">
+
+                <!-- start page title -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box d-flex align-items-center justify-content-between">
+                            <h4 class="mb-0 font-size-18">Starter</h4>
+
+                            <div class="page-title-right">
+                                <ol class="breadcrumb m-0">
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Paginas</a></li>
+                                    <li class="breadcrumb-item active">Arrancador</li>
+                                </ol>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- end page title -->
+
+            </div> <!-- container-fluid -->
+            <div class="container my-5">
+                <div class="row">
+                    <div class="col-md-4 d-flex justify-content-center align-items-center">
+                        <img src="imagen/nosotros3333.webp" class="img-fluid" alt="Imagen de Nosotros">
+                    </div>
+                    <div class="col-md-8">
+                        <h2 class="text-center">SOBRE NOSOTROS</h2>
+                        <p class="text-justify ">
+                            En nuestra tienda virtual de zapatillas, nos dedicamos a ofrecerte mucho más que simples
+                            zapatos. Somos un destino para aquellos que buscan expresarse a
+                            través de su calzado, para los que desean caminar con confianza y originalidad en cada paso
+                            que dan.
+
+                            En Zapatillas Únicas, valoramos la individualidad y la diversidad de estilos. Nuestra
+                            cuidadosa selección de calzado no solo abarca
+                            las últimas tendencias de moda, sino también diseños únicos y exclusivos que te permitirán
+                            destacar entre la multitud.
+                        </p>
+                        <p class="text-justify">
+                            Cada par de zapatillas que ofrecemos cuenta una historia, desde su diseño innovador hasta su
+                            fabricación con materiales de la más alta calidad. Nos esforzamos por brindarte una
+                            experiencia de compra en línea sin complicaciones, donde puedas explorar, descubrir y
+                            encontrar
+                            ese par de zapatillas que no solo completen tu outfit, sino que también reflejen tu
+                            personalidad y estilo.
+                        </p>
+
+                    </div>
+                </div>
+
+
             </div>
+            <!-- End Page-content -->
+
+
         </div>
-        <?php include ('include/pie_pagina.php'); ?>
-    </div>
+        <!-- end main content-->
 
+    </div>
+    <!-- END layout-wrapper -->
+
+
+    <?php include ('include/pie_pagina.php'); ?>
     <!-- jQuery  -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.4.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
